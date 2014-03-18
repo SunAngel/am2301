b9adb7bbe9e2b252a6700b469e0f1aa1fe1776f0
root <root@Potte.(none)>
2014-03-18 22:54:17 +0400

    Add proc entry for reads

--- am2301.c
+++ am2301.c
@@ -38,9 +38,10 @@ struct st_inf {
 #define SHORT_DELAY 3
 //#define DEFAULT_DELAY 5
 #define DEFAULT_DELAY 30
-#define PROC_SUBDIR "am2301"
+#define MODULE_NAME "am2301"
+#define PROC_SUBDIR MODULE_NAME
 
-static int _pin = 24;
+static int _pin = 27;
 static int _read_delay = DEFAULT_DELAY; /* in seconds */
 static int _irq = -1;
 static volatile int _read_req = READ_STOP;
@@ -53,7 +54,7 @@ static unsigned int _reads[2] = {0, 0};
 static unsigned char _data[5];
 
 #ifdef CONFIG_PROC_FS
-static struct proc_dir_entry *am231_dir = NULL, /* *pin_dir = NULL,*/ *temp_ent = NULL, *rh_ent = NULL;
+static struct proc_dir_entry *am231_dir = NULL, /* *pin_dir = NULL,*/ *temp_ent = NULL, *rh_ent = NULL, *reads_ent = NULL;
 
 static int am231_show_temp(struct seq_file *m, void *v)
 {
@@ -94,6 +95,24 @@ static int am231_open_rh(struct inode *inode, struct  file *file) {
 static const struct file_operations fops_rh = {
 	.owner = THIS_MODULE,
 	.open = am231_open_rh,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+static int am231_show_reads(struct seq_file *m, void *v)
+{
+	seq_printf(m, "%d/%d\n", _reads[0], _reads[1]);
+	return 0;
+}
+
+static int am231_open_reads(struct inode *inode, struct  file *file) {
+	return single_open(file, am231_show_reads, NULL);
+}
+
+static const struct file_operations fops_reads = {
+	.owner = THIS_MODULE,
+	.open = am231_open_reads,
 	.read = seq_read,
 	.llseek = seq_lseek,
 	.release = single_release,
@@ -287,7 +306,7 @@ static int __init am2301_init(void)
 {
 	int ret;
 
-	printk(KERN_INFO "Init am2301\n");
+	printk(KERN_INFO "Init " MODULE_NAME "\n");
 
 	ret = gpio_request_one(_pin, GPIOF_OUT_INIT_HIGH, "AM2301");
 
@@ -298,7 +317,7 @@ static int __init am2301_init(void)
 
 	_irq =  gpio_to_irq(_pin);
 	if (_irq < 0) {
-		printk(KERN_ERR "am2301: Unable to create IRQ\n");
+		printk(KERN_ERR MODULE_NAME ": Unable to create IRQ\n");
 		goto _cleanup_1;
 
 	}
@@ -309,12 +328,12 @@ static int __init am2301_init(void)
 			  IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING,
 			  "read_isr", NULL);
 
-	ts = kthread_create(read_thread, NULL, "am2301");
+	ts = kthread_create(read_thread, NULL, MODULE_NAME);
 
 	if (ts) {
 		wake_up_process(ts);
 	} else {
-		printk(KERN_ERR "am2301: Unable to create thread\n");
+		printk(KERN_ERR MODULE_NAME": Unable to create thread\n");
 		goto _cleanup_2;
 	}
 
@@ -323,19 +342,25 @@ static int __init am2301_init(void)
 	
 	am231_dir = proc_mkdir(PROC_SUBDIR, NULL);
 	if (!am231_dir) {
-		printk(KERN_ERR "am2301: Unable to create /proc/" PROC_SUBDIR "\n");
+		printk(KERN_ERR MODULE_NAME ": Unable to create /proc/" PROC_SUBDIR "\n");
 		goto _cleanup_3;
 	}
 
 	temp_ent = proc_create("temp", 0, am231_dir, &fops_temp);
 	if (!temp_ent) {
-		printk(KERN_ERR "am2301: Unable to create /proc/" PROC_SUBDIR "/temp\n");
+		printk(KERN_ERR MODULE_NAME ": Unable to create /proc/" PROC_SUBDIR "/temp\n");
 		goto _cleanup_4;
 	}
 
 	rh_ent = proc_create("rh", 0, am231_dir, &fops_rh);
 	if (!rh_ent) {
-		printk(KERN_ERR "am2301: Unable to create /proc/" PROC_SUBDIR "/rh\n");
+		printk(KERN_ERR MODULE_NAME ": Unable to create /proc/" PROC_SUBDIR "/rh\n");
+		goto _cleanup_4;
+	}
+
+	reads_ent = proc_create("reads", 0, am231_dir, &fops_reads);
+	if (!reads_ent) {
+		printk(KERN_ERR MODULE_NAME ": Unable to create /proc/" PROC_SUBDIR "/reads\n");
 		goto _cleanup_4;
 	}
 #endif
@@ -352,6 +377,10 @@ _cleanup_4:
 	}
 	if (rh_ent) {
 		proc_remove(rh_ent);
+		rh_ent = NULL;
+	}
+	if (reads_ent) {
+		proc_remove(reads_ent);
 		rh_ent = NULL;
 	}
 _cleanup_3:
@@ -389,7 +418,11 @@ static void __exit am2301_exit(void)
 		proc_remove(rh_ent);
 		rh_ent = NULL;
 	}
-	printk(KERN_INFO "am2301: exit\n");
+	if (reads_ent) {
+		proc_remove(reads_ent);
+		rh_ent = NULL;
+	}
+	printk(KERN_INFO MODULE_NAME ": exit\n");
 }
 
 module_init(am2301_init);
@@ -400,4 +433,4 @@ MODULE_AUTHOR("Constantin Petra");
 MODULE_DESCRIPTION("AM2301 driver");
 module_param(_pin, int, S_IRUGO);
 //MODULE_PARM(pin, "i");
-MODULE_PARM_DESC(_pin, "Pin number - if not set, assuming 24");
+MODULE_PARM_DESC(_pin, "Pin number - if not set, assuming 27");
